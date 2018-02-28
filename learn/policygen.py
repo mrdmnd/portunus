36ae239ca5ac0170801b55238c18cca417365487
Matt Redmond <mrdmnd@gmail.com>
2018-02-27 21:52:06 -0800

    tiny changes

--- learn/policygen.py
+++ learn/policygen.py
@@ -3,14 +3,16 @@ from __future__ import division
 from __future__ import print_function
 
 import sys
+import os
 import grpc
 import tensorflow as tf
 
 from absl import app
 from absl import flags
 from absl import logging
+
 from google.protobuf.text_format import MessageToString
-from proto import encounter_config_pb2
+
 from proto import policy_pb2
 from proto import simulation_pb2
 from proto import service_pb2
@@ -26,34 +28,8 @@ flags.DEFINE_string('port', '50051', '')
 flags.DEFINE_integer('sim_max_iterations', 10000, '')
 flags.DEFINE_float('sim_target_error', 0.01, '')
 
-flags.DEFINE_string('encounter_textproto',
-                    'examples/encounters/encounter_patchwork.textproto',
-                    'A .textproto file containing an encounter_config.')
-
-flags.DEFINE_string('player_textproto',
-                    'examples/players/player_synecdoche.textproto',
-                    'A  .textproto file containing a player_config.')
-
-flags.DEFINE_string('player_simcdump', '',
-                    'A .simcdump file containing player gear information.')
-
 
 def main(argv):
-    # Ensure exactly one encounter builder and one player builder are present.
-    # if FLAGS.encounter_textproto and ... :
-    if FLAGS.player_textproto and FLAGS.player_simcdump:
-        logging.error("Error: >1 arguments provided for player configuration.")
-        return 1
-
-    encounter_factory = encounter_builder.Textproto(FLAGS.encounter_textproto)
-    if FLAGS.player_textproto:
-        player_factory = player_builder.Textproto(FLAGS.player_textproto)
-    elif FLAGS.player_simcdump:
-        player_factory = player_builder.SimcReport(FLAGS.player_simcdump)
-    else:
-        logging.error("Need to pass in at least one player builder flag.")
-        return 1
-
     # Sanity checking our environment.
     logging.info('Running Python {0[0]}.{0[1]}.{0[2]}'.format(
         sys.version_info))
@@ -64,9 +40,33 @@ def main(argv):
     logging.info('Tensorflow has CUDA-supporting GPU available: {0}'.format(
         tf.test.is_gpu_available(cuda_only=True)))
 
-    # Setup communication with RPC simulation service.
+    # Ensure parseable configs for encounter and player exist.
+    if len(argv) != 3:
+        logging.error("You must pass in an encounter and a player file.")
+        return 1
+
+    # Try to setup communication with RPC simulation service.
     channel = grpc.insecure_channel(FLAGS.host + ":" + FLAGS.port)
     stub = service_pb2_grpc.SimulationServiceStub(channel)
+
+    # Setup factory for encounter and player configs.
+    encounter_path = argv[1]
+    encounter_extension = os.path.splitext(encounter_path)[1]
+    if encounter_extension == ".textproto":
+        encounter_factory = encounter_builder.Textproto(encounter_path)
+    else:
+        logging.error("I don't know how to parse that kind of encounter file.")
+        return 1
+
+    player_path = argv[2]
+    player_extension = os.path.splitext(player_path)[1]
+    if player_extension == ".textproto":
+        player_factory = player_builder.Textproto(player_path)
+    elif player_extension == ".simcdump":
+        player_factory = player_builder.SimcReport(player_path)
+    else:
+        logging.error("I don't know how to parse that kind of player file.")
+        return 1
 
     # Load encounter and player configurations.
     encounter_config = encounter_factory.Build()
@@ -74,7 +74,7 @@ def main(argv):
     player_config = player_factory.Build()
     logging.info("PlayerConfig:\n" + MessageToString(player_config))
 
-    # Fixed part of simulation configuration (encounter, player, static params)
+    # Const part of simulation configuration (encounter, player, static params)
     fixed_simulation_config = simulation_pb2.SimulationConfig()
     fixed_simulation_config.max_iterations = FLAGS.sim_max_iterations
     fixed_simulation_config.target_error = FLAGS.sim_target_error
