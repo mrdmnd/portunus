74fc9634aa47bcc0637f09942220594f210daba1
mrdmnd <mredmond@strava.com>
2019-01-12 12:28:39 -0800

    Add front end.

---
+++ frontend/portunus.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+import requests
+DEBUG = True
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+def show_homepage():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG)
