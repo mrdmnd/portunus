f1b607792c0f4ba4b5daa53c898f67e6a649225f
Matt Redmond <mttrdmnd@gmail.com>
2020-05-18 03:16:00 -0700

    Add datamining subdir

---
+++ datamining/src/casc.rs
@@ -0,0 +1,4 @@
+// CASC file formats, based on the work of Caali et al.
+// http://www.ownedcore.com/forums/world-of-warcraft/world-of-warcraft-model-editing/471104-analysis-of-casc-filesystem.html
+// Ported from Simulationcraft to Rust: https://github.com/simulationcraft/simc/blob/bfa-dev/casc_extract/casc.py
+
