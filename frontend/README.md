76f77b8b3559149f7a083efe6acfd210f42946a6
Matt Redmond <mttrdmnd@gmail.com>
2020-06-05 23:41:23 -0700

    [frontend] Website updates, login/reg forms

--- frontend/README.md
+++ frontend/README.md
@@ -1,15 +1,26 @@
 The website for portunuspl.us
 
 # Develop:
+If you haven't produced a virtualenv yet, make one:
+
+    python3 -m venv venv
+
 Ensure you are in the venv
+
     . venv/bin/activate
 
 Ensure dependencies are up-to-date
-    pip install -t lib -r requirements.txt --upgrade
+
+    pip3 install -t lib -r requirements.txt --upgrade
 
 The -t lib flag copies the libraries into a lib folder, which is uploaded to App
 Engine during deployment. The -r requirements.txt flag tells pip to install
 everything from that file.
 
+If you haven't installed google cloud SDK, install it:
+    curl https://sdk.cloud.google.com | bash
+
+
 To deploy, run
+
     gcloud app deploy
--- frontend/main.py
+++ frontend/main.py
@@ -1,12 +1,57 @@
-from flask import Flask, request, render_template
-DEBUG = True
+import os
+from flask import Flask, request, render_template, send_from_directory, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['SECRET_KEY'] = 'e38425d0dcea98645bee3b0f3f3aaf5b'
+
+dungeon_routes = [
+    {
+        'title': 'mrdmnd rox',
+        'creator': 'mrdmnd',
+        'date_created': '2020-01-01',
+        'content': 'bloohblahlulwkekw'
+    }    
+]
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'icons/favicon.ico')
+
 
 @app.route('/')
-def show_homepage():
-    return render_template('index.html')
+@app.route('/home')
+def home():
+    return render_template('index.html', dungeon_routes = dungeon_routes)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for user {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if (form.email.data == 'admin@portunuspl.us' and form.password.data == 'password'):
+            flash('Logged into account!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check Email and Password.', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+
 
 if __name__ == '__main__':
-    app.run(debug=DEBUG)
+    app.run(debug=True)
